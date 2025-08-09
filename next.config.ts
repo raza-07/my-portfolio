192d4848711b52c824dd9fb8664fa69d1e894e37
mohsink10 <mohsinkamran867@gmail.com>
2025-08-09 20:03:57 +0500

    Disable ESLint during Vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // This skips ESLint errors during Vercel build
+  },
 };
 
 export default nextConfig;
